34ce8447264e4761627f535adbac3c007a72232b
raiden00pl <raiden00@railab.me>
2025-01-30 09:40:20 +0100

    conf.py: update author

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "raiden00"  # (translatable)
+BLOG_AUTHOR = "raiden00 <Mateusz Szafoni>"  # (translatable)
 BLOG_TITLE = "www.railab.me"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
